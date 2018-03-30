7bc0489f84150ddd4c1f4a7885b0ae8f849cfc05
Irina Evsei <irinaevsei.ie@gmail.com>
2018-03-30 17:41:43 +0300

    some problems with add element in a modal view. //TO DO

--- src/test/java/edu/bsuir/test/TestExample.java
+++ src/test/java/edu/bsuir/test/TestExample.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import sun.awt.windows.WEmbeddedFrame;
 
 
 public class TestExample {
@@ -41,10 +42,85 @@ public class TestExample {
     }
 
     @Test
-    public void createRequest(){
+    public void addApplication(){
         driver.get("http://testing.cld.iba.by/web/guest/recruiting");
         WebElement request = driver.findElement(By.xpath("//*[@id=\"addRequest\"]/div[2]/div[2]/a"));
         request.click();
         Assert.assertEquals("Создание заявки на подбор персонала - Конструктор Талантов",driver.getTitle());
+    }
+
+    @Test
+    public void createApplication(){
+        driver.get("http://testing.cld.iba.by/web/guest/recruiting/applications/-/applications/createApplication");
+
+        WebElement position = driver.findElement(By.xpath("//*[@id=\"name\"]"));
+        position.sendKeys("Дизайнер");
+
+        WebElement plannedClosingDate = driver.findElement(By.xpath("//*[@id=\"plannedClosingDate\"]"));
+        plannedClosingDate.sendKeys("11.04.2018");
+
+        WebElement priorityRadioBtn = driver.findElement(By.xpath("//*[@id=\"priority-1\"]"));
+        priorityRadioBtn.click();
+
+        WebElement quantity = driver.findElement(By.xpath("//*[@id=\"quantity\"]"));
+        quantity.sendKeys("2");
+
+        WebElement creationReason = driver.findElement(By.xpath("//*[@id=\"creationReason\"]"));
+        creationReason.sendKeys("НАМ НУЖНЫ ДИЗАЙНЕРЫ !");
+
+        WebElement type = driver.findElement(By.xpath("//*[@id=\"contractType-2\"]"));
+        type.click();
+
+        WebElement salary = driver.findElement(By.xpath("//*[@id=\"salary\"]"));
+        salary.sendKeys("500");
+
+        WebElement employees = driver.findElement(By.xpath("//*[@id=\"employees\"]"));
+        employees.sendKeys("Присутсвуют");
+
+        WebElement businessTrip = driver.findElement(By.xpath("//*[@id=\"businessTrip\"]"));
+        businessTrip.sendKeys("США, 3 месяца");
+
+        WebElement timetable = driver.findElement(By.xpath("//*[@id=\"timetable\"]"));
+        timetable.sendKeys("40 часов");
+
+        WebElement probationPeriod = driver.findElement(By.xpath("//*[@id=\"probationPeriod\"]"));
+        probationPeriod.sendKeys("2 месяца");
+
+        WebElement responsibilities = driver.findElement(By.xpath("//*[@id=\"responsibilities\"]"));
+        responsibilities.sendKeys("WORK HARD,  PLAY HARD");
+
+        WebElement candidateType = driver.findElement(By.xpath("//*[@id=\"candidateTypeInternal\"]"));
+        candidateType.click();
+
+        WebElement educationSpecialization = driver.findElement(By.xpath("//*[@id=\"educationSpecialization\"]"));
+        educationSpecialization.sendKeys("Студенты 3-4 курса приветствуются");
+
+        WebElement priorityWorkingExperience = driver.findElement(By.xpath("//*[@id=\"priority.working.experience\"]"));
+        priorityWorkingExperience.sendKeys("не имеет значения");
+
+        WebElement undesirableWorkingExperience = driver.findElement(By.xpath("//*[@id=\"undesirableWorkingExperience\"]"));
+        undesirableWorkingExperience.sendKeys("не имеет значения");
+
+        WebElement editrequiredCompetence = driver.findElement(By.xpath("//*[@id=\"editrequiredCompetence\"]"));
+        editrequiredCompetence.click();
+        WebElement input = driver.findElement(By.xpath("//*[@id=\"searchCompetenceWord\"]"));
+        input.sendKeys("Обучаемость");
+        WebElement addCompetence = driver.findElement(By.cssSelector("//*[contains(@class, 'icon-arrow-right')]\n"));
+        addCompetence.click();
+//        WebElement basicCompetence = driver.findElement(By.xpath("//*[@id=\"category_22\"]"));
+//        basicCompetence.click();
+//        WebElement addCompetence = driver.findElement(By.xpath("//*[@id=\"yui_patched_v3_11_0_3_1522413201989_8743\"]"));
+//        addCompetence.click();
+        WebElement modalOkBtn = driver.findElement(By.xpath("//*[@id=\"yui_patched_v3_11_0_3_1522413201989_8910\"]"));
+        modalOkBtn.click();
+
+        WebElement comment = driver.findElement(By.xpath("//*[@id=\"comment\"]"));
+        comment.sendKeys("Нам нужны самые-самые! =)");
+
+        WebElement saveBtn = driver.findElement(By.xpath("//*[@id=\"save\"]"));
+        saveBtn.click();
+
+        WebElement message = driver.findElement(By.xpath("//*[@id=\"successMessage\"]"));
+        Assert.assertEquals("Заявка успешно сохранена",message.getText());
     }
 }
